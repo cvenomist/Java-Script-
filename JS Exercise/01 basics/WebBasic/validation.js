ef1032e8b2f259ad0ca0f3462de2d26acaaf02af
Sujay Singh <38438205+cvenomist@users.noreply.github.com>
2018-12-18 00:39:06 +0530

    Update validation.js

--- JS Exercise/01 basics/WebBasic/validation.js
+++ JS Exercise/01 basics/WebBasic/validation.js
@@ -1,10 +1,20 @@
 function myValidation() {
-    let myValue = document.getElementById('myForm').value;
+    let myValue = document.getElementById('myform').value;
 
-    if (isNAN(myValue) || myValue < 1 || myValue > 20) {
+    if (myValue > 10 || myValue > 20) {
         console.log("Not A Valid Input");
     } else {
         console.log("Input Is OK");
         
     }
 }
+
+//Form Validation
+
+document.querySelector('.myform').addEventListener('submit', (event)=>{
+    event.preventDefault();
+    console.log(event.target.username.value);
+    console.log(event.target.realname.value);
+    event.target.username.value = '';
+    event.target.realname.value = '';
+})
