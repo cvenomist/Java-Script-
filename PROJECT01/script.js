2210ad7c5440bc3a9557383d97748167f224e508
Sujay Singh <38438205+cvenomist@users.noreply.github.com>
2018-12-18 01:34:49 +0530

    Add files via upload

---
+++ PROJECT01/script.js
@@ -0,0 +1,17 @@
+// alert('Hello')
+
+/*
+var h = document.createElement('h1')
+var myValue = document.createTextNode('Hello World!');
+
+h.appendChild(myValue)
+document.querySelector('h1').appendChild(h);
+*/
+
+/*While Loop Concept
+var val = 5;
+while (val > 0) {
+    console.log(val);
+    val--;
+}
+*/
