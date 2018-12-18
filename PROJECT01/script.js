0d525fbf1c4ce6f19d198c45b14aa8203d7acdfc
Sujay Singh <38438205+cvenomist@users.noreply.github.com>
2018-12-18 11:11:14 +0530

    Update script.js

--- PROJECT01/script.js
+++ PROJECT01/script.js
@@ -15,3 +15,58 @@ while (val > 0) {
     val--;
 }
 */
+
+var ul = document.getElementById('list');
+var li;
+
+var addButton = document.getElementById('add');
+addButton.addEventListener('click', addItem);
+
+var removeButton = document.getElementById('remove');
+removeButton.addEventListener('click', removeItem);
+
+function addItem() {
+    var input = document.getElementById('input');
+    var item = input.value;
+    ul = document.getElementById('list');
+    var textnode = document.createTextNode(item)
+
+    if (item === '') {
+        return false;
+        //Add P tag And Assign Value Enter Your To Do
+    } else {
+        //Create Li
+        li = document.createElement('li');
+        //Create Checkbox
+        var checkbox = document.createElement('input')
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id', 'check');
+
+        //Create Label
+        var label = document.createElement('label');
+        label.setAttribute('for', 'item') //Optional
+
+        //Add These Element To Web Page
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(textnode);
+        li.appendChild(label);
+        ul.insertBefore(li, ul.childNodes[0]);
+        //li.className = 'visual';
+
+        setTimeout(() => {
+            li.className = 'visual';
+        }, 4);
+        //For Clearing The Input After Insert
+        input.value = '';
+    }
+}
+
+function removeItem () {
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+        while (li[index] && li[index].children[0].checked) {
+            ul.removeChild(li[index])
+        }
+    }
+}
